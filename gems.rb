b898e62b2d29c30592c4114a9b15296cbff05582
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-02-09 00:57:14 +1300

    Specify minimum `ddtrace` version.

--- gems.rb
+++ gems.rb
@@ -21,4 +21,10 @@ group :test do
 	
 	gem "sus"
 	gem "covered"
+	
+	# `::Datadog::Tracing::Utils::TraceId.to_low_order` is introduced in 1.10.0:
+	# https://github.com/DataDog/dd-trace-rb/pull/2543/files#diff-634ece3c2962088040f7d60ec5c895212f6083ac241310a0c5bf0edb645d300f
+	# But `Identifier#trace_id` which is used by the tests is introduced in 1.13.0:
+	# https://github.com/DataDog/dd-trace-rb/pull/2974
+	gem "ddtrace", "~> 1.13"
 end
