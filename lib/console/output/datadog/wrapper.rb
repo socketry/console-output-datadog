1b3c2a3de6d7b418ae5b6dc21f1dbff46c82a661
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-02-09 00:37:20 +1300

    Use 64-bit trace_ids as required by the current implementation of Datadog. (#4)

--- lib/console/output/datadog/wrapper.rb
+++ lib/console/output/datadog/wrapper.rb
@@ -27,6 +27,8 @@ require 'ddtrace'
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+# frozen_string_literal: true
+
 module Console
 	module Output
 		# The reason why this is a serialized logger rather than an output filter, is because it needs to directly modify the top level of the record.
@@ -41,16 +43,23 @@ module Console
 						if span = trace.active_span
 							options[:dd] = {
 								span_id: span.id.to_s,
-								trace_id: trace.id.to_s
+								trace_id: format_trace_id(trace.id)
 							}
 						else
 							options[:dd] = {
-								trace_id: trace.id.to_s
+								trace_id: format_trace_id(trace.id)
 							}
 						end
 					end
 					
 					@output.call(subject, *arguments, **options, &block)
+				end
+				
+				private
+				
+				def format_trace_id(id)
+					# 128-bit tracing is not supported by the Datadog agent, so we need to convert it to 64-bit. We expect that this will be changed in the future.
+					::Datadog::Tracing::Utils::TraceId.to_low_order(id)
 				end
 			end
 		end
